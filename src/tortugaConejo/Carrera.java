24ed4b4bfb39c554ba0d14e5dfb4cbbea3a6e41a
Daniel <Daniel@SALON.home>
2022-12-03 17:27:17 +0100

    código terminado y documentado. He cambiado distancia a constante

--- src/tortugaConejo/Carrera.java
+++ src/tortugaConejo/Carrera.java
@@ -1,20 +1,23 @@
 package tortugaConejo;
 
 public class Carrera {
-	public static int distancia = 10;
-	private Corredor corredor1;
-	private Corredor corredor2;
+	// atributos
+	public static final int distancia = 10; // común a todas las carreras y constante.
 
-	public Carrera(String corr1, String corr2) {
+	private Corredor corredor1; // corredor1 de la instancia carrera generada
+	private Corredor corredor2; // corredor2 de la instancia carrera generada
+
+	public Carrera(String corr1, String corr2) { // constructor
 		corredor1 = new Corredor(corr1);
 		corredor2 = new Corredor(corr2);
 	}
 
 	public void empiezaCarrera(String tipoCarrera) {
-		if (tipoCarrera.equals("normal")) {
-			corredor1.start();
+
+		if (tipoCarrera.equals("normal")) { // carrera tipo normal
+			corredor1.start(); // corredores empiezan a la vez
 			corredor2.start();
-			try {
+			try { // la instancia carrera espera a q los corredores terminen
 				corredor1.join();
 				corredor2.join();
 			} catch (InterruptedException e) {
@@ -23,31 +26,34 @@ public class Carrera {
 			System.out.println("Carrera " + tipoCarrera + " entre " + corredor1.getCorredor() + " Y "
 					+ corredor2.getCorredor() + " ha terminado");
 
-		} else if (tipoCarrera.equals("conejoEspera")) {
-			corredor1.start();
-			try {
+		} else if (tipoCarrera.equals("conejoEspera")) { // carrera tipo conejoEspera
+			corredor1.start(); // empieza primero tortuga
+			try { // hilo principal espera a que termine para continuar
 				corredor1.join();
 			} catch (InterruptedException e) {
 				e.printStackTrace();
-			}
+			} // cuando hilo principal continúa una vez terminada tortuga empieza conejo.
 			corredor2.start();
 			try {
-				corredor2.join();
+				corredor2.join(); // hilo principal espera a conejo para terminar toda la carrera.
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 			System.out.println("Carrera " + tipoCarrera + " entre " + corredor1.getCorredor() + " Y "
 					+ corredor2.getCorredor() + " ha terminado");
-		} else {
+		} else { // por si se introduce una carrera de un tipo erróneo o desconocido.
 			System.out.println("Error en tipo de Carrera");
 		}
 	}
 
 	public static void main(String[] args) {
 
+		// empezamos carrera tipo normal.
 		Carrera carrera1 = new Carrera("Tortuga", "Conejo");
 		carrera1.empiezaCarrera("normal");
 
+		// empezamos carrera tipo conejoEspera. Son dos instancias de carrera
+		// diferentes.
 		Carrera carrera2 = new Carrera("Tortuga", "Conejo");
 		carrera2.empiezaCarrera("conejoEspera");
 	} // fin main
--- src/tortugaConejo/Corredor.java
+++ src/tortugaConejo/Corredor.java
@@ -2,26 +2,30 @@ package tortugaConejo;
 
 public class Corredor extends Thread {
 
-	private String corredor;
-	private int velocidad;
+	private String corredor; // nombre del corredor.
+	private int velocidad; // velocidad de cada corredor.
 
-	public Corredor(String corredor) {
+	public Corredor(String corredor) { // constructor
 		this.corredor = corredor;
-
+		// dependiendo del corredor la velocidad será una u otra
 		if (corredor.equals("Tortuga")) {
 			velocidad = 5000; // milisegundos
 		} else if (corredor.equals("Conejo")) {
 			velocidad = 1000; // milisegundos
-		} else {
+		} else { // si hay error en el corredor pasado como parámetro
 			System.out.println("error en el corredor");
 		}
 	}
 
 	public void run() {
-
+		// empieza a correr el corredor.
 		System.out.println(corredor + " comienza carrera");
 
 		try {
+			/*
+			 * recorremos la distancia de la carrera durmiendo al corredor velocidad miliseg
+			 * e indicando en cada iteración la posición en la que se encuentra.
+			 */
 			for (int posicion = 1; posicion < (Carrera.distancia + 1); posicion++) {
 				Thread.sleep(velocidad);
 				System.out.println(corredor + " va por " + posicion + " de la carrera.");
@@ -29,7 +33,7 @@ public class Corredor extends Thread {
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-
+		// acaba la carrera el corredor
 		System.out.println(corredor + " finaliza la carrera");
 
 	} // fin run
