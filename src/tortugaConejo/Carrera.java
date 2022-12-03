837aeb88822e25b0b9c82be97586d76ac17c65e7
Daniel <Daniel@SALON.home>
2022-12-03 12:05:55 +0100

    preparo para hacer más carreras y hago distancia variable

--- src/tortugaConejo/Carrera.java
+++ src/tortugaConejo/Carrera.java
@@ -1,29 +1,30 @@
 package tortugaConejo;
 
 public class Carrera {
-	public static final int distancia = 10;
+	public static int distancia = 10;
 	private Corredor corredor1;
 	private Corredor corredor2;
 
-	public Carrera(String corredor1, String corredor2) {
+	public Carrera(String corredor1, String corredor2, int dist) {
 		this.corredor1 = new Corredor(corredor1);
 		this.corredor2 = new Corredor(corredor2);
+		distancia = dist;
 		this.corredor1.start();
 		this.corredor2.start();
+
+		// espero a que ambos corredores terminen para terminar la carrera
 		try {
 			this.corredor1.join();
 			this.corredor2.join();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-
-		System.out.println("LA CARRERA HA TERMINADO");
+		// hasta q no terminan ambos corredores su ejecución no continuo
+		System.out.println("LA CARRERA ENTRE " + corredor1 + " Y " + corredor2 + " HA TERMINADO");
 	}
 
-	
-
 	public static void main(String[] args) {
-		Carrera carrera = new Carrera("Tortuga", "Conejo");
+		Carrera carrera = new Carrera("Tortuga", "Conejo", 10);
 	} // fin main
 
 } // fin Carrera
